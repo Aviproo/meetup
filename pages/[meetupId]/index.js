5273f2c6e5f5be0d59439178c0d0b7e8f1f26c4c
AVINASH KUMAR <Aviproo>
2023-12-28 13:16:56 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,4 +10,39 @@ function MeetUpDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/e3/Allegheny_Monongahela_Ohio.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some stree 5, some city",
+        escription: "This is a first meetup",
+      },
+    },
+  };
+}
 export default MeetUpDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,43 @@
 import MeetupList from "../components/meetups/MeetupList";
 
-function HomePage() {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "A first Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Allegheny_Monongahela_Ohio.jpg",
-      address: "Some address 5, sre zone ",
-      description: "This is a first meetup",
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e3/Allegheny_Monongahela_Ohio.jpg",
+    address: "Some address 5, sre zone ",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e3/Allegheny_Monongahela_Ohio.jpg",
+    address: "Some 2nd address 5, pro zone ",
+    description: "This is second meetup",
+  },
+];
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
     },
-    {
-      id: "m2",
-      title: "second Meetup",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/e/e3/Allegheny_Monongahela_Ohio.jpg",
-      address: "Some 2nd address 5, pro zone ",
-      description: "This is second meetup",
-    },
-  ];
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+    revalidate: 1,
+  };
 }
 export default HomePage;
